d53ada9dac6543c5504576be73067fbcec0d5a03
Marcel Lauhoff <ml@irq0.org>
2016-05-18 17:11:37 +0200

    Add tool: Get ceph cluster stats in graphite format

---
+++ cluster_stats_graphite.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+
+"""
+Print Ceph cluster stats in graphite format
+"""
+
+import sys
+import time
+import json
+import re
+import rados
+
+from ceph_argparse import json_command
+
+def ceph_get_pg_stats(rados_handle):
+    """
+    Wrapper for Ceph 'pg stat' json command
+    """
+    ret, j, _ = json_command(rados_handle, prefix="pg stat",
+                           argdict={'format':'json'})
+
+    if ret == 0:
+        return json.loads(j)
+
+def pg_stats(rados_handle):
+    """
+    Return cluster pg stats
+    """
+
+    # see PGMap.cc/PGMonitor.cc
+    interesting_stats_keys = (
+        # standard stats
+        "read_bytes_sec",
+        "write_bytes_sec",
+        "io_sec",
+        "raw_bytes",
+        "raw_bytes_avail",
+        "raw_bytes_used",
+        "num_bytes",
+        "num_pgs",
+        # recovery rate summary
+        "recovering_objects_per_sec",
+        "recovering_bytes_per_sec",
+        "recovering_keys_per_sec",
+        "num_objects_recovered",
+        "num_bytes_recovered",
+        "num_keys_recovered",
+        # recovery_summary
+        "unfound_objects",
+        "unfound_total",
+        "unfound_ratio",
+        "misplaced_objects",
+        "misplaced_total",
+        "misplaced_ratio",
+        "degraded_objects",
+        "degraded_total",
+        "degraded_ratio",
+    )
+
+    data = ceph_get_pg_stats(rados_handle)
+
+    if data:
+        metrics = [("pgstat.{}".format(k), data.get(k, 0))
+                   for k in interesting_stats_keys]
+
+        pg_by_state = [("pg_by_state.{}".format(x["name"]), x["num"])
+                       for x in data["num_pg_by_state"]]
+        metrics.extend(pg_by_state)
+
+        return metrics
+
+def sanitize_name(name):
+    """
+    Remove chars that don't work well in Graphite metric names
+    """
+    return re.sub(r"[^\w]", "_", name)
+
+def rados_stat(rados_handle):
+    """
+    Return stats from RADOS API (pool and cluster)
+    """
+    pools = rados_handle.list_pools()
+
+    cluster_stats = rados_handle.get_cluster_stats()
+
+    metrics = [("cluster.{}".format(sanitize_name(k)), v)
+               for k, v in cluster_stats.iteritems()]
+
+    for pool in pools:
+        io_ctx = rados_handle.open_ioctx(pool)
+        pool_stat = [("pool.{}.{}".format(sanitize_name(pool),
+                                          sanitize_name(k)), v)
+                     for k, v in io_ctx.get_stats().iteritems()]
+
+        metrics.extend(pool_stat)
+        io_ctx.close()
+
+    return metrics
+
+def print_graphite(data, now, prefix):
+    """
+    Print data in sequence in graphite format
+    """
+    for key, val in data:
+        print "{}.{}".format(prefix, key), val, now
+
+def setup(conf):
+    rados_handle = rados.Rados(conffile=conf)
+    rados_handle.connect()
+    return rados_handle
+
+def main():
+    rados_handle = setup(conf=sys.argv[1])
+
+    print_graphite(pg_stats(rados_handle), int(time.time()), "ceph.cluster")
+    print_graphite(rados_stat(rados_handle), int(time.time()), "ceph.cluster")
+
+if __name__ == "__main__":
+    main()
